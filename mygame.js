3f2e16ea145bcc49ea0a904d8d775f4042bfe2b2
Deepanshu Tyagi <47669231+DeepanshTyagi@users.noreply.github.com>
2020-05-27 21:28:50 +0530

    Update mygame.js
    This is not an Final Commit

--- mygame.js
+++ mygame.js
@@ -1,6 +1,14 @@
+/*
+Created By Deepanshu Tyagi on 27/05/2020
+*/
 var game=()=>
 {
     // HEre we write Basic Skeleton of Phareser Object 
+    var prize_config=
+    {
+        count:11,
+        prize_names:["0%","10%","20%","30%","40%","50%","60%","70%","80%","90%","100%"]
+    }
     let config=
         {
             type:Phaser.CANVAS,
@@ -27,8 +35,8 @@ var game=()=>
         //console.log(this);
         // now we load the images 
         this.load.image('background','background.jpg');
-        this.load.image('pin','pin.jpg');
-        this.load.image('wheel','wheel.png');
+        this.load.image('pin','pin.png');
+        this.load.image('wheel','FinalWheel.png');
     }
     function create()
     {
@@ -42,14 +50,27 @@ var game=()=>
         background.setScale(.80);//reduce the size of the image 
         
         // Now create a wheel on the background 
-        let wheel=this.add.sprite(0,0,'wheel');
-        wheel.setPosition(W/2,H/2);
-        wheel.setScale(.20);
+        this.wheel=this.add.sprite(W/2,H/2,'wheel');
+        this.wheel.setPosition(W/2,H/2);
+        this.wheel.setScale(.50);
         
         // Now create a pin 
         let pin=this.add.sprite(0,0,'pin');
         pin.setPosition((W/2)-10,(H/2)-80);
         pin.setScale(.05);
+        
+        // adding a event =listener for mouse click 
+        this.input.on("pointerdown",spinwheel,this);
+        
+        // adding the welcone text
+        font_style=
+        {
+            font:"bold 15px Roboto",
+            align:"center",
+            color:"red"
+            
+        };
+        this.game_text=this.add.text(5,5,"Welcome to Spin and Win",font_style);
         
         
     }
@@ -57,5 +78,42 @@ var game=()=>
     function update()
     {
         console.log("In update Function");
+        this.wheel.angle+=2; // with the help of this we will rotate the spin 
+      //  this.wheel.alpha-=.01; // alpha 1 means oppase
+                            // alpha 0 means transparent
+        /*this.wheel.scaleX+=.001; 
+        this.wheel.scaleY+=.001; // from scaleX and scaleY wheel becaome larger 
+        */
     }
+    function spinwheel()
+    
+    {
+        console.log("You Clicked the mouse");
+        this.game_text.setText("You Clicked on Mouse"); // Update the text
+        
+        let round=Phaser.Math.Between(2,4);
+        console.log(round);
+        let degree=Phaser.Math.Between(0,10)*32.72;
+        
+        let total_angel=round*360+degree;
+        console.log(degree);
+        
+        let idx=prize_config.count-1-Math.floor(degree/(360/prize_config.count));
+        
+        tween = this.tweens.add({
+            targets:this.wheel,
+            angle:total_angel,
+            ease:"Cubic.easeOut",
+            duration : 3000,
+            callbackScope:this,
+//            scaleX:0.5,
+//            scaleY:0.5,
+            onComplete:function()
+            {
+            this.game_text.setText("Congratulations! You Won "+prize_config.prize_names[idx]);
+            }
+        });  
+    }
+    
+    //--> Tweens are used to create animation in Phaser 
 }
